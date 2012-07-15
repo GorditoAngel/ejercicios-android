57d4e4cc660ae6a7b14bedfd1e34f7df813661c1
Ismael.garciaserrano <Ismael.garciaserrano@gmail.com>
2012-07-15 03:01:31 +0000

    Funcionacon dos dedos pero hay mucho que aprender sobre el multitouch

--- AplicacionSencilla/src/com/especialcursos/ui/AplicacionSencillaActivity.java
+++ AplicacionSencilla/src/com/especialcursos/ui/AplicacionSencillaActivity.java
@@ -1,9 +1,11 @@
 package com.especialcursos.ui;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.TextView;
@@ -15,18 +17,13 @@ public class AplicacionSencillaActivity extends Activity {
 	TextView textView_velocidad = null;
 	TextView textView_dxdy = null;
 	
-	private int x_i;
-	private int y_i;
+	TextView textView2 = null;
+	TextView textView_velocidad2 = null;
 	
-	private int x_f;
-	private int y_f;
+	private ArrayList<Dedito> lista_dedos = null;
 	
-	private float x_mov;
-	private float y_mov;
-	private Calendar instante = null;
 	
-	Calendar tiempoInicio = null;
-	Calendar tiempoFinal = null;
+	Resources res;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -35,90 +32,115 @@ public class AplicacionSencillaActivity extends Activity {
 		setContentView(R.layout.main);
 		
 		textView = (TextView) findViewById(R.id.main_textView);
-		textView.setText("No has pulsado todavia");
 		textView_velocidad = (TextView) findViewById(R.id.main_textView_velocidad);
 		textView_dxdy = (TextView) findViewById(R.id.main_textView_dxdy);
+		
+		textView2 = (TextView) findViewById(R.id.main_textView2);
+		textView_velocidad2 = (TextView) findViewById(R.id.main_textView_velocidad2);
+		
+		res = getResources();
+		lista_dedos = new ArrayList<Dedito>();
+		
+		//Como maximo 2 dedos
+		for (int i=0; i < 2; i++){
+			Dedito dedillo = new Dedito();
+			lista_dedos.add(dedillo);
+		}
 		
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		int index = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK)
+				>> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
 		final int action = event.getAction();
-		switch (action & MotionEvent.ACTION_MASK) {
-			case MotionEvent.ACTION_DOWN:
-				tiempoInicio = Calendar.getInstance();
-				x_mov = (float)event.getX();
-				y_mov = (float)event.getY();
-				x_i = (int) x_mov;
-				y_i = (int) y_mov;
-				instante = Calendar.getInstance();
-				break;
-			case MotionEvent.ACTION_MOVE:{
-				final float x = event.getX();
-				final float y = event.getY();
-				Calendar final_mov = Calendar.getInstance();
-				//distancia movida
-				final float dx = x - x_mov;
-				final float dy = y - y_mov;
-				long dt = (final_mov.getTimeInMillis() - 
-						instante.getTimeInMillis());
-				
-				instante = Calendar.getInstance();
-				x_mov = x;
-				y_mov = y;
-				
-				//Escribir resultado
-				textView_dxdy.setText("(dx,dy) = ("+ dx+ ","+dy+"), " +
-						"(Vx,Vy) = ("+ Float.toString(dx*1000F/dt)+ 
-						","+ Float.toString(dy*1000F/dt)+"px/s). \n" +
-						"dt = " + dt + "ms." );
-				
-				
-				break;
+		if (index < 2){
+			Dedito dedito_pulsado = lista_dedos.get(index);
+			
+			switch (action & MotionEvent.ACTION_MASK) {
+				case MotionEvent.ACTION_DOWN:
+					dedito_pulsado.setID(event.getPointerId(index));
+					dedito_pulsado.setTiempoInicio(Calendar.getInstance());
+					dedito_pulsado.setX_mov((float)event.getX());
+					dedito_pulsado.setY_mov((float)event.getY());
+					dedito_pulsado.setX_i((int) dedito_pulsado.getX_mov());
+					dedito_pulsado.setY_i((int) dedito_pulsado.getY_mov());
+					dedito_pulsado.setInstante(Calendar.getInstance());
+					break;
+				case MotionEvent.ACTION_POINTER_DOWN:
+					dedito_pulsado.setID(event.getPointerId(index));
+					dedito_pulsado.setTiempoInicio(Calendar.getInstance());
+					dedito_pulsado.setX_mov((float)event.getX());
+					dedito_pulsado.setY_mov((float)event.getY());
+					dedito_pulsado.setX_i((int) dedito_pulsado.getX_mov());
+					dedito_pulsado.setY_i((int) dedito_pulsado.getY_mov());
+					dedito_pulsado.setInstante(Calendar.getInstance());
+					break;
+				case MotionEvent.ACTION_MOVE:{
+					final float x = event.getX(index);
+					final float y = event.getY(index);
+					Calendar final_mov = Calendar.getInstance();
+					//distancia movida
+					final float dx = x - lista_dedos.get(index).getX_mov();
+					final float dy = y - lista_dedos.get(index).getY_mov();
+					long dt = (final_mov.getTimeInMillis() - 
+							lista_dedos.get(index).getInstante().getTimeInMillis());
+					lista_dedos.get(index).setInstante(Calendar.getInstance());
+					lista_dedos.get(index).setX_mov(x);
+					lista_dedos.get(index).setY_mov(y);
+					//escribir movimientos
+					escribirMovimiento(dx, dy, dt);
+					break;
+				}
+				case MotionEvent.ACTION_UP:
+				case MotionEvent.ACTION_POINTER_UP:
+					dedito_pulsado.setTiempoFinal(Calendar.getInstance());
+					dedito_pulsado.setX_f((int)event.getX());
+					dedito_pulsado.setY_f((int)event.getY());
+					//escribir resultados
+					escribirResultado_dedo(dedito_pulsado);
+					escribirVelocidad(dedito_pulsado);
+					break;
 			}
-			case MotionEvent.ACTION_UP:
-				tiempoFinal = Calendar.getInstance();
-				x_f = (int)event.getX();
-				y_f = (int)event.getY();
-				escribirResultado(x_i,y_i,x_f,y_f);
-				
-//				Intent intent = new Intent (
-//						AplicacionSencillaActivity.this,
-//						AplicacionSencillaResults.class);
-//				Bundle bundle = new Bundle();
-//				bundle.putLong("TIEMPOPRESIONADO",	
-//						tiempoFinal.getTimeInMillis() -
-//						tiempoInicio.getTimeInMillis());
-//				bundle.putInt("X", x);
-//				bundle.putInt("Y", y);
-//				
-//				intent.putExtras(bundle);
-//				startActivity(intent);
-				break;
 		}
 	return true;
 	}
 	
-	private void escribirResultado(int xi, int yi, int xf,int yf){
-		String s_x = Integer.toString(xi);
-		String s_y = Integer.toString(yi);
-		String s_xf = Integer.toString(xf);
-		String s_yf = Integer.toString(yf);
-		String Ax = Integer.toString(xf-xi);
-		String Ay = Integer.toString(yf-yi);
-		
-		long t1 = (tiempoInicio != null)?(tiempoInicio.getTimeInMillis()):0;
-		long t2 = (tiempoFinal != null)?(tiempoFinal.getTimeInMillis()):0;
+	private void escribirResultado_dedo(Dedito dedito_pulsado){
+		long t1 = (dedito_pulsado.getTiempoInicio() != null)?(dedito_pulsado.getTiempoInicio().getTimeInMillis()):0;
+		long t2 = (dedito_pulsado.getTiempoFinal() != null)?(dedito_pulsado.getTiempoFinal().getTimeInMillis()):0;
 		String diferencia = Long.toString(t2-t1);
-		
-		float distancia = (float) Math.sqrt(Math.pow(xf-xi,2)+Math.pow(yf-yi,2));
-		
-		textView.setText("Has pulsado la pantalla durante " 
-				+ diferencia + " ms. Desde las coordenadas (x1,y1) = (" + 
-				s_x + "," + s_y +") a las coordenadas (x2,y2) = (" +
-				s_xf + "," + s_yf +"). (\u0394x,\u0394y) = ("+Ax+","+Ay+").");
-		textView_velocidad.setText("vel. media = " + 
-				Float.toString(distancia/((t2-t1)/1000F))+ " px/s.");
+		if (dedito_pulsado.getID() == 0)
+			textView.setText(res.getString(R.string.has_pulsado) + diferencia + 
+					"ms. "+ res.getString(R.string.desde) +" (x1,y1) = (" 
+						+ dedito_pulsado.getX_i() + "," + dedito_pulsado.getY_i() +") " +
+					"a (x2,y2) = ("+ dedito_pulsado.getX_f() + "," + dedito_pulsado.getY_f() +").") ;
+		else
+			textView2.setText(res.getString(R.string.has_pulsado) + diferencia + 
+					"ms. "+ res.getString(R.string.desde) +" (x1,y1) = (" 
+					+ dedito_pulsado.getX_i() + "," + dedito_pulsado.getY_i() +") " +
+					"a (x2,y2) = ("+ dedito_pulsado.getX_f() + "," + dedito_pulsado.getY_f() +").") ;
+	}
+	private void escribirVelocidad(Dedito dedito_pulsado){
+		long t1 = (dedito_pulsado.getTiempoInicio() != null)?(dedito_pulsado.getTiempoInicio().getTimeInMillis()):0;
+		long t2 = (dedito_pulsado.getTiempoFinal() != null)?(dedito_pulsado.getTiempoFinal().getTimeInMillis()):0;
+		float X = dedito_pulsado.getX_f() - dedito_pulsado.getX_i();
+		float Y = dedito_pulsado.getY_f() - dedito_pulsado.getY_i();
+		float distancia = (float) Math.sqrt(Math.pow(X,2)+Math.pow(X,2));
+		if (dedito_pulsado.getID() == 0)
+			textView_velocidad.setText("(\u0394x,\u0394y) = ("+X+","+Y+"). " + "\n" + res.getString(R.string.vel_med) 
+					+ Float.toString(distancia/((t2-t1)/1000F))+ " px/s.");
+		else
+			textView_velocidad2.setText("(\u0394x,\u0394y) = ("+X+","+Y+"). " + "\n" + res.getString(R.string.vel_med) 
+					+ Float.toString(distancia/((t2-t1)/1000F))+ " px/s.");
 		
 	}
+	private void escribirMovimiento(float dx, float dy, float dt){
+		textView_dxdy.setText("(dx,dy) = ("+ dx+ ","+dy+"), \n" +
+				"(Vx,Vy) = ("+ Float.toString(dx*1000F/dt)+ 
+				","+ Float.toString(dy*1000F/dt)+")px/s. \n" +
+				"dt = " + dt + "ms." );
+
+	}
+	
 }
--- AplicacionSencilla/src/com/especialcursos/ui/Dedito.java
+++ AplicacionSencilla/src/com/especialcursos/ui/Dedito.java
@@ -1,5 +1,101 @@
 package com.especialcursos.ui;
 
+import java.util.Calendar;
+
 public class Dedito {
-	private int pointerIndex;
+	private int pointerID;
+	private int x_i,y_i,x_f,y_f;
+	
+	private float x_mov;
+	private float y_mov;
+	private Calendar instante= null;
+	
+	private Calendar tiempoInicio = null;
+	private Calendar tiempoFinal = null;
+	
+	public Dedito(int index){
+		pointerID = index;
+	}
+	
+	public Dedito(){
+	}
+	
+	public int getID(){
+		return pointerID;
+	}
+	public void setID(int id){
+		pointerID = id;
+	}
+
+	public int getX_i() {
+		return x_i;
+	}
+
+	public void setX_i(int x_i) {
+		this.x_i = x_i;
+	}
+
+	public int getY_i() {
+		return y_i;
+	}
+
+	public void setY_i(int y_i) {
+		this.y_i = y_i;
+	}
+
+	public int getX_f() {
+		return x_f;
+	}
+
+	public void setX_f(int x_f) {
+		this.x_f = x_f;
+	}
+
+	public int getY_f() {
+		return y_f;
+	}
+
+	public void setY_f(int y_f) {
+		this.y_f = y_f;
+	}
+
+	public float getX_mov() {
+		return x_mov;
+	}
+
+	public void setX_mov(float x_mov) {
+		this.x_mov = x_mov;
+	}
+
+	public float getY_mov() {
+		return y_mov;
+	}
+
+	public void setY_mov(float y_mov) {
+		this.y_mov = y_mov;
+	}
+
+	public Calendar getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Calendar instante) {
+		this.instante = instante;
+	}
+
+	public Calendar getTiempoInicio() {
+		return tiempoInicio;
+	}
+
+	public void setTiempoInicio(Calendar tiempoInicio) {
+		this.tiempoInicio = tiempoInicio;
+	}
+
+	public Calendar getTiempoFinal() {
+		return tiempoFinal;
+	}
+
+	public void setTiempoFinal(Calendar tiempoFinal) {
+		this.tiempoFinal = tiempoFinal;
+	}
 }
