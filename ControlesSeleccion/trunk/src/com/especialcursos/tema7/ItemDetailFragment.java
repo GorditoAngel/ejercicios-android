d3a15202b6abdaa6aa751afd70370937af444ed1
ismael.garciaserrano@gmail.com <ismael.garciaserrano@gmail.com@48a61585-d805-13d1-16c5-d7665236c51f>
2012-07-17 00:15:21 +0000

    casi lo tengo

--- ControlesSeleccion/trunk/src/com/especialcursos/tema7/ItemDetailFragment.java
+++ ControlesSeleccion/trunk/src/com/especialcursos/tema7/ItemDetailFragment.java
@@ -2,11 +2,16 @@ package com.especialcursos.tema7;
 
 import com.especialcursos.tema7.dummy.DummyContent;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ItemDetailFragment extends Fragment {
@@ -31,9 +36,34 @@ public class ItemDetailFragment extends Fragment {
             Bundle savedInstanceState) {
     	
     	//TODO Creoq eu aqui es donde hay que seleccionar lso layouts
-        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+        View rootView = new View(getActivity());
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
+        	 
+        	//TODO Creoq eu aqui es donde hay que seleccionar lso layouts
+        	if (mItem.content.equals("Spinner")){
+        		rootView = inflater.inflate(R.layout.control_spinner, container, false);
+        		Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner_detail);
+        		// Create an ArrayAdapter using the string array and a default spinner layout
+        		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+        		        R.array.fisicos_array, android.R.layout.simple_spinner_item);
+        		// Specify the layout to use when the list of choices appears
+        		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        		// Apply the adapter to the spinner
+        		spinner.setAdapter(adapter);
+        	}else if (mItem.content.equals("List View")){
+        		rootView = inflater.inflate(R.layout.control_listview, container, false);
+        		ListView list = (ListView) rootView.findViewById(R.id.list_view_detail);
+        		// Create an ArrayAdapter using the string array and a default spinner layout
+        		ListAdapter<CharSequence> adapter = ListAdapter.createFromResource(getActivity(),
+        		        R.array.fisicos_array, android.R.layout.simple_spinner_item);
+        		// Specify the layout to use when the list of choices appears
+        		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        		// Apply the adapter to the spinner
+        		list.setAdapter(adapter);
+        	}else{
+        		rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+        		((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
+        	}
         }
         return rootView;
     }
