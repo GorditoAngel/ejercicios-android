458a567cc6056f86182f8def74ce2c34d263a78e
Ismael.garciaserrano <Ismael.garciaserrano@gmail.com>
2012-07-17 16:16:57 +0000

    ControlesExtendidos - Falta avisar de cuando se llega a 250 caracters

--- ControlesExtendidos/src/igserrano/control/controlesextendidos/ControlesExtendidosActivity.java
+++ ControlesExtendidos/src/igserrano/control/controlesextendidos/ControlesExtendidosActivity.java
@@ -2,12 +2,41 @@ package igserrano.control.controlesextendidos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.TextView;
 
 public class ControlesExtendidosActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	EditTextContador control_personalizado;
+	TextView  textView_control;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        control_personalizado = (EditTextContador) findViewById(R.id.controlPersonalizado);
+        textView_control = (TextView) findViewById(R.id.textView1);
+        
+        control_personalizado.addTextChangedListener(new TextWatcher() {
+			
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				textView_control.setText("onTextChanged");
+			}
+			
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				textView_control.setText("beforeTextChanged");
+			}
+			
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				textView_control.setText("afterTextChanged");
+			}
+		});
     }
 }
