3c948e4b21ca1f6973ea29922a1657d21670fde2
Ismael.garciaserrano <Ismael.garciaserrano@gmail.com>
2012-07-18 23:56:24 +0000

    MenusEjercicio - queda la parte de listactivity

--- MenusEjercicio/trunk/src/com/especialcursos/tema9/MainActivity.java
+++ MenusEjercicio/trunk/src/com/especialcursos/tema9/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -13,11 +13,46 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+    	getMenuInflater().inflate(R.menu.menu_main_activity, menu);
         return true;
+    }
+    
+    @Override
+    /**
+     * Oyente del menu
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()){
+    	case R.id.sub1_radio1:
+    	case R.id.sub1_radio2:
+    	case R.id.sub1_radio3:
+    	case R.id.sub1_radio4:
+    		item.setChecked(true);
+    		break;
+    	case R.id.sub1_checkbox1:
+    	case R.id.sub1_checkbox2:
+    		if (item.isChecked())
+    			item.setChecked(false);
+    		else
+    			item.setChecked(true);
+    		break;
+    	case R.id.sub2_item1:
+    	case R.id.sub2_item2:
+    	case R.id.sub2_item3:
+    		goToListActivity(item);
+    	default:
+    		break;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+    
+    
+    public void goToListActivity(MenuItem item){
+    	//TODO enviar a la otra actividad
+    	Toast.makeText(this, "funciona (amndar a listActivity", Toast.LENGTH_LONG).show();
     }
 
     
