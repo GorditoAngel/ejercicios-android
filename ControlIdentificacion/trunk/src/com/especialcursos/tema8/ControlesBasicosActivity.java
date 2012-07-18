5dfb1cbe6019045f2b55f21da70b6342e341c711
Ismael.garciaserrano <Ismael.garciaserrano@gmail.com>
2012-07-18 16:17:28 +0000

    Controlidentificacion - terminado

--- ControlIdentificacion/trunk/src/com/especialcursos/tema8/ControlesBasicosActivity.java
+++ ControlIdentificacion/trunk/src/com/especialcursos/tema8/ControlesBasicosActivity.java
@@ -3,8 +3,7 @@ package com.especialcursos.tema8;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+
 
 public class ControlesBasicosActivity extends Activity {
 
