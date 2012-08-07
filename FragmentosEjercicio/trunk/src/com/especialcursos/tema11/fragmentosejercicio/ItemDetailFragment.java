d95b924f7e3a82690260458ba08248c80d2eee40
Ismael.garciaserrano <Ismael.garciaserrano@gmail.com>
2012-08-07 12:23:18 +0000

    FragmentosEjercicio - con errores

--- FragmentosEjercicio/trunk/src/com/especialcursos/tema11/fragmentosejercicio/ItemDetailFragment.java
+++ FragmentosEjercicio/trunk/src/com/especialcursos/tema11/fragmentosejercicio/ItemDetailFragment.java
@@ -2,7 +2,10 @@ package com.especialcursos.tema11.fragmentosejercicio;
 
 import com.especialcursos.tema11.fragmentosejercicio.dummy.DummyContent;
 
+import android.content.CursorLoader;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +25,16 @@ public class ItemDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments().containsKey(ARG_ITEM_ID)) {
+        	Uri uri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+    		String[] proyeccion = { MediaStore.MediaColumns._ID,
+    		MediaStore.MediaColumns.TITLE,
+    		MediaStore.MediaColumns.MIME_TYPE,
+    		MediaStore.MediaColumns.DATE_ADDED,
+    		MediaStore.MediaColumns.SIZE};
+    		String select = "";
+    		String ordenacion = MediaStore.MediaColumns.TITLE + " DESC";
+    		return new CursorLoader(getActivity(), uri, proyeccion, select, null,
+    				ordenacion);
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
         }
     }
--- FragmentosEjercicio/trunk/src/com/especialcursos/tema11/fragmentosejercicio/ItemListFragment.java
+++ FragmentosEjercicio/trunk/src/com/especialcursos/tema11/fragmentosejercicio/ItemListFragment.java
@@ -2,42 +2,72 @@ package com.especialcursos.tema11.fragmentosejercicio;
 
 import com.especialcursos.tema11.fragmentosejercicio.dummy.DummyContent;
 
-import android.R;
 import android.app.Activity;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.app.LoaderManager;
 
-public class ItemListFragment extends ListFragment {
+
+public class ItemListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String STATE_ACTIVATED_POSITION = "activated_position";
+    private static final int ID_LOADER = 2001;
 
     private Callbacks mCallbacks = sDummyCallbacks;
     private int mActivatedPosition = ListView.INVALID_POSITION;
+    
+    
+    private SimpleCursorAdapter mAdapter;
 
     public interface Callbacks {
 
-        public void onItemSelected(String id);
+        public void onItemSelected(String id, String name);
     }
 
     private static Callbacks sDummyCallbacks = new Callbacks() {
         
-        public void onItemSelected(String id) {
+        public void onItemSelected(String id, String name) {
         }
     };
 
     public ItemListFragment() {
     }
+    
+    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
-                R.layout.simple_list_item_activated_1,
-                R.id.text1,
-                DummyContent.ITEMS));
+        
+        String[] columnas = { MediaStore.MediaColumns.TITLE,
+        		MediaStore.MediaColumns.MIME_TYPE,
+        		MediaStore.MediaColumns.DATE_ADDED,
+        		MediaStore.MediaColumns.SIZE };
+        int[] contenedor_items = { android.R.id.text1,
+        		android.R.id.text2};
+        
+        mAdapter = new SimpleCursorAdapter(getActivity(),
+        		android.R.layout.simple_list_item_1,
+        		null,
+        		columnas,
+                contenedor_items,0);
+        
+        setListAdapter(mAdapter);
+        
+        //No moestramos la lista de momento
+        setListShown(false);
+        
+        //prepara el loader
+        getLoaderManager().initLoader(ID_LOADER, null, (LoaderCallbacks<Cursor>) this);
     }
 
     @Override
@@ -68,7 +98,7 @@ public class ItemListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView listView, View view, int position, long id) {
         super.onListItemClick(listView, view, position, id);
-        mCallbacks.onItemSelected(DummyContent.ITEMS.get(position).id);
+        mCallbacks.onItemSelected(Long.toString(id), listView.getItemAtPosition(position).toString());
     }
 
     @Override
@@ -94,4 +124,39 @@ public class ItemListFragment extends ListFragment {
 
         mActivatedPosition = position;
     }
+
+
+
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		Uri uri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+		String[] proyeccion = { MediaStore.MediaColumns._ID,
+		MediaStore.MediaColumns.TITLE,
+		MediaStore.MediaColumns.MIME_TYPE,
+		MediaStore.MediaColumns.DATE_ADDED,
+		MediaStore.MediaColumns.SIZE};
+		String select = "";
+		String ordenacion = MediaStore.MediaColumns.TITLE + " DESC";
+		return new CursorLoader(getActivity(), uri, proyeccion, select, null,
+				ordenacion);
+	}
+
+
+
+	public void onLoadFinished(Loader<Cursor> loader, Cursor datos) {
+		mAdapter.swapCursor(datos);
+		
+		//muestra la lista
+		if (isResumed()) {
+            setListShown(true);
+        } else {
+            setListShownNoAnimation(true);
+        }
+	}
+
+
+
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		mAdapter.swapCursor(null);
+		
+	}
 }
