a5a3b21cfc346c56e852c9992d192b361a5a5c0b
ismael.garciaserrano@gmail.com <ismael.garciaserrano@gmail.com@48a61585-d805-13d1-16c5-d7665236c51f>
2012-11-21 21:59:28 +0000

    finish

--- NumerosFactoriales/trunk/src/com/especialcursos/tema12/procesos/CalcularFactorialesTask.java
+++ NumerosFactoriales/trunk/src/com/especialcursos/tema12/procesos/CalcularFactorialesTask.java
@@ -1,11 +1,14 @@
 package com.especialcursos.tema12.procesos;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import android.os.AsyncTask;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
 public class CalcularFactorialesTask extends
-		AsyncTask<Integer, Long, Long> {
+		AsyncTask<Integer, BigInteger, Long> {
 
 	private ProgressBar progreso;
 	private EditText etResultado;
@@ -15,11 +18,26 @@ public class CalcularFactorialesTask extends
 		etResultado = ctxt.getETResultados();
 	}
 	
-	public long factorial(int n){
+	public double factorial(int n){
 		if (n == 0)
 			return 1;
 		else
 			return n*factorial(n-1);
+	}
+	
+	//este aguanta mas numeros
+	//porque necesita menos memoria
+	public BigInteger factorialNorecursivo(int n){
+		if (n == 0){
+			return new BigInteger("1");
+		}else{
+			BigInteger resultado =  new BigInteger("1");
+			for (int i = 1; i < (n+1) ; i++){
+				resultado = resultado.multiply(new BigInteger(Integer.toString(i)));
+			}
+			return resultado;
+		}
+		
 	}
 	
 	@Override
@@ -32,6 +50,7 @@ public class CalcularFactorialesTask extends
 	@Override
 	protected Long doInBackground(Integer ... arg0) {
 		int numero_factoriales = arg0[0];
+		BigInteger indice;
 		for (int ii = 0; ii < numero_factoriales; ii++){
 			synchronized (this) {
 				try {
@@ -41,17 +60,19 @@ public class CalcularFactorialesTask extends
 					e.printStackTrace();
 				}
 			}
-			publishProgress((long) (((ii+1)/ (float) numero_factoriales)*100), factorial(ii + 1));
+			indice = new BigInteger(Integer.toString((ii +1)));
+			publishProgress(new BigInteger(Integer.toString( (int) (((ii+1)/ (double) numero_factoriales)*100))), factorialNorecursivo(ii + 1), indice);
 		}
 		return null;
 	}
 	
 	@Override
-	protected void onProgressUpdate(Long... values) {
-		long progr = values[0]; 
-		long factorial = values[1];
-		progreso.setProgress((int) progr);
-		etResultado.append(Long.toString(factorial) + ", ");
+	protected void onProgressUpdate(BigInteger... values) {
+		BigInteger progr = values[0]; 
+		BigInteger factorial = values[1];
+		BigInteger indice = values[2];
+		progreso.setProgress((int) progr.longValue());
+		etResultado.append(indice.toString() +":  " + factorial.toString() + "\n");
 	}
 
 }
