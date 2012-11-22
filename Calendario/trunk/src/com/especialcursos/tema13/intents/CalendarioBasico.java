882ff2a0418920061ecfc09e07457dd214a00c8e
Ismael.garciaserrano@gmail.com <Ismael.garciaserrano@gmail.com@48a61585-d805-13d1-16c5-d7665236c51f>
2012-11-22 13:34:27 +0000

    importacion inicial

---
+++ Calendario/trunk/src/com/especialcursos/tema13/intents/CalendarioBasico.java
@@ -0,0 +1,24 @@
+package com.especialcursos.tema13.intents;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class CalendarioBasico extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendario_basico);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_calendario_basico, menu);
+        return true;
+    }
+
+    
+}
