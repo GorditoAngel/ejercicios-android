922ff84b51886f74ee7856fb683cdfeef72465ce
Ismael.garciaserrano@gmail.com <Ismael.garciaserrano@gmail.com@48a61585-d805-13d1-16c5-d7665236c51f>
2012-11-23 17:07:50 +0000

    estamos haciendo la base de datos de citas

--- Calendario/trunk/src/com/especialcursos/tema13/intents/BDCalendario.java
+++ Calendario/trunk/src/com/especialcursos/tema13/intents/BDCalendario.java
@@ -1,5 +1,84 @@
 package com.especialcursos.tema13.intents;
 
-public class BDCalendario {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.sql.Date;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BDCalendario extends SQLiteOpenHelper{
+	
+	static final String NOMBRE_DB = "calendario_db";
+	static final String TB_CALENDARIO = "Calendario";
+	static final String CITA_ID = "cita_id";
+	static final String CITA_DESCRIPCION = "descripcion";
+	static final String CITA_LUGAR = "lugar";
+	static final String CITA_FECHA = "fecha";
+	static final String CITA_AVISAR = "avisar";
+	
+	private String crear_tabla= "CREATE TABLE " + TB_CALENDARIO
+			+ " (" + CITA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
+			+ CITA_DESCRIPCION + " TEXT, "
+			+ CITA_LUGAR + " TEXT, "
+			+ CITA_FECHA + " TIMESTAMP NOT NULL DEFAULT current_timestamp, "
+			+ CITA_AVISAR + " INTEGER)";
+	
+	public BDCalendario(Context context){
+		super (context, NOMBRE_DB, null, 1);
+	}
+	
+	public BDCalendario(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// Creación de la base de datos y tabla
+		db.execSQL(crear_tabla);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		//Se elimina la versión anterior de la tabla
+        db.execSQL("DROP TABLE IF EXISTS " + TB_CALENDARIO);
+        //Se crea la nueva versión de la tabla
+        db.execSQL(crear_tabla);
+		
+	}
+	
+	private void agregarDatosIniciales(SQLiteDatabase db){
+		ArrayList<Cita> citas = new ArrayList<Cita>();
+		citas.add(new Cita(0, "tomar un cafe", "Startbucks de Atocha",
+				 new Date(), 10));
+	}
+	
+	private void agregarCitas(ArrayList<Cita> citas){
+		
+	}
+	
+	public void agregarCita(Cita cita){
+		
+	}
+	
+	public void borrarCita(Cita cita){
+		
+	}
+	
+	public void borrarCita(int id){
+		
+	}
+	
+	public Cita getCita(int id){
+		return null;
+	}
+	
+	public ArrayList<Cita> getCitas(){
+		return null;
+	}
+	
 }
--- Calendario/trunk/src/com/especialcursos/tema13/intents/Cita.java
+++ Calendario/trunk/src/com/especialcursos/tema13/intents/Cita.java
@@ -1,5 +1,60 @@
 package com.especialcursos.tema13.intents;
 
-public class Cita {
+import java.sql.Date;
 
+public class Cita {
+	private int _id;
+	private String _descripcion;
+	private String _lugar;
+	private Date _fecha;
+	private int _avisar;
+	
+	public Cita(int id, String _descripcion, String _lugar, Date _fecha, int _avisar) {
+		super();
+		this._id = id;
+		this._descripcion = _descripcion;
+		this._lugar = _lugar;
+		this._fecha = _fecha;
+		this._avisar = _avisar;
+	}
+
+	public int get_id() {
+		return _id;
+	}
+
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+
+	public String get_descripcion() {
+		return _descripcion;
+	}
+
+	public void set_descripcion(String _descripcion) {
+		this._descripcion = _descripcion;
+	}
+
+	public String get_lugar() {
+		return _lugar;
+	}
+
+	public void set_lugar(String _lugar) {
+		this._lugar = _lugar;
+	}
+
+	public Date get_fecha() {
+		return _fecha;
+	}
+
+	public void set_fecha(Date _fecha) {
+		this._fecha = _fecha;
+	}
+
+	public int get_avisar() {
+		return _avisar;
+	}
+
+	public void set_avisar(int _avisar) {
+		this._avisar = _avisar;
+	}
 }
